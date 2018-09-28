31095486da46ca16f3d8f016226cb945db35e889
Sam Greaves <samgreaves0@gmail.com>
2018-09-28 14:28:03 +0100

    Populate header file

---
+++ UI/src/App/Components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export class Header extends React.Component{
+    render() {
+        return(
+            <nav className="navbar navbar-default">
+                <div className="container">
+                    <ul className="nav navbar-nav">
+                        <li><a href="#">Home</a></li>
+                    </ul>
+                </div>
+            </nav>
+        );
+    }
+}
