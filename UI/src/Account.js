c039fa5d6aad63ca0c26ff0b0d465c65977b5493
Sam Greaves <samgreaves0@gmail.com>
2018-09-28 11:54:01 +0100

    front end main page now displays accounts in grid form

---
+++ UI/src/Account.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            response: []
+        };
+    }
+
+    componentDidMount(){
+
+        fetch('account/5ba4c5dabf9d9a91d482cbc4')
+            .then(res => res.json())
+            .then(json => {this.setState({response: json}); console.log(json)});
+
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h1 className="App-title">Welcome to Accounts</h1>
+                </header>
+                <p className="App-intro">
+                    Accounts
+                    <div>
+                        <ul>
+                            {this.state.response.map(el => (
+                                <li>
+                                    Name: {el['account-name']}
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                </p>
+            </div>
+        );
+    }
+}
+
+export default App;
--- UI/src/App.js
+++ UI/src/App.js
@@ -1,20 +1,42 @@
 import React, { Component } from 'react';
-import React from 'react';
-import Link from 'react-router';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
+    constructor() {
+        super();
+    this.state = {
+        response: []
+    };
+}
+
+  componentDidMount(){
+
+      fetch('accounts')
+          .then(res => res.json())
+          .then(json => {this.setState({response: json}); console.log(json)});
+
+  }
+
+    render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to Accounts</h1>
         </header>
-        <p className="App-intro">
-            <Link to="/accounts">Accounts</Link>
-        </p>
+            Accounts
+            <div class="grid-container">
+                    {this.state.response.map(el => (
+                        <div class="grid-item">
+                            <p>
+                            Name: {el['account-name']}
+                            <br/>{el['account-amount']}
+                            </p>
+                        </div>
+                    ))}
+
+            </div>
       </div>
     );
   }
