df213577d20dfe38e0635230f2209a01cfe186a1
Sam Greaves <samgreaves0@gmail.com>
2019-03-29 17:51:21 +0000

    MA-4 fix stray s char in tools file

--- Tools/PullLocally.sh
+++ Tools/PullLocally.sh
@@ -13,4 +13,4 @@ docker pull samgreaves/moneyappbackend:latest
 echo "Image pulled from docker successfully"
 docker run -d -p 4000:4000 --link mongo:mongo --env MONGODB_URL=mongodb://mongo:27017/moneyapp samgreaves/moneyappbackend:latest
 
-echo "MoneyApp backend running on http://localhost:4000"s
+echo "MoneyApp backend running on http://localhost:4000"
