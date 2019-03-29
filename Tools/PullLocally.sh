6e63d3b61d94d47d62e1027dfdc38c4bc04a928e
Sam Greaves <samgreaves0@gmail.com>
2019-03-29 16:42:00 +0000

    MA-23 use latest in pull sh tool

--- Tools/PullLocally.sh
+++ Tools/PullLocally.sh
@@ -7,10 +7,10 @@ error() {
     exit 1
 }
 
-echo "Pulling image samgreaves/moneyappbackend:0.1.$1"
-docker pull samgreaves/moneyappbackend:0.1.$1
+echo "Pulling image samgreaves/moneyappbackend:latest"
+docker pull samgreaves/moneyappbackend:latest
 
 echo "Image pulled from docker successfully"
-docker run -d -p 4000:4000 --link mongo:mongo --env MONGODB_URL=mongodb://mongo:27017/moneyapp samgreaves/moneyappbackend:0.1.$1
+docker run -d -p 4000:4000 --link mongo:mongo --env MONGODB_URL=mongodb://mongo:27017/moneyapp samgreaves/moneyappbackend:latest
 
-echo "MoneyApp backend running on http://localhost:4000"
+echo "MoneyApp backend running on http://localhost:4000"s
